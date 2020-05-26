d38930bca5e8d2344c7ab3982de4250d87a859a3
amartin527 <63210605+amartin527@users.noreply.github.com>
2020-05-26 16:18:09 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-west-2",
+      BUCKET: "dog-ear-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-west-2",
+      URL: "https://2vp2512ez9.execute-api.us-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-west-2",
+      USER_POOL_ID: "us-west-2_41Cb6JZCy",
+      APP_CLIENT_ID: "6vc72fa6tr18sg2kpi2bjh5gul",
+      IDENTITY_POOL_ID: "us-west-2:941a687d-4139-44d9-ac87-d50cbb32530d"
+    }
+  };
