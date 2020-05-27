4019b0102038a16206eca67dc1746878818670c0
amartin527 <63210605+amartin527@users.noreply.github.com>
2020-05-26 20:19:26 -0700

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
       REGION: "us-west-2",
       BUCKET: "dog-ear-app-uploads"
@@ -12,5 +13,6 @@ export default {
       USER_POOL_ID: "us-west-2_41Cb6JZCy",
       APP_CLIENT_ID: "6vc72fa6tr18sg2kpi2bjh5gul",
       IDENTITY_POOL_ID: "us-west-2:941a687d-4139-44d9-ac87-d50cbb32530d"
-    }
+    },
+    STRIPE_KEY: "pk_test_DE7MEwB3KqWgFsITYvjJ2dnB001iBAoilx",
   };
